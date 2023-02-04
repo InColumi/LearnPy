f20e202f5a7e2bf84f4533ffa44a4d36dc17c373
InColumi <max77648@gmail.com>
2023-02-04 17:28:20 +0300

    Delete all

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title('Лабораторная работа 4')
-
-uploaded_file = st.file_uploader("Choose a photo")
-if uploaded_file is not None:
-    # To read file as bytes:
-    with st.spinner('Wait for it...'):
-        bytes_data = uploaded_file.getvalue()
-        st.image(bytes_data)
-    st.success('Done!')
-    st.snow()
+print('Hi for CI/CD')
